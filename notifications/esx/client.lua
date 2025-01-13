1a80d7cd8be1765adecf6c15b3d505ab48e7dbdc
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2025-01-13 18:48:03 +0100

    feat(notifications/esx/client): types

--- notifications/esx/client.lua
+++ notifications/esx/client.lua
@@ -1,5 +1,8 @@
 No_OxBridge = {}
 
+---@param message string
+---@param type? 'info' | 'success' | 'error'
+---@param duration? number
 function No_OxBridge:Notify(message, type, duration)
     local ESX = Fw_ESXBridge:ESX()
     if not message or message == '' then return end
