07318ce384355f072d1623b32fe7ae59c40c0fc1
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2025-01-13 18:44:25 +0100

    feat(frameworks/esx/client): return type

--- frameworks/esx/client.lua
+++ frameworks/esx/client.lua
@@ -1,6 +1,7 @@
 Fw_ESXBridge = {}
 Fw_ESXBridge.ESX = exports["es_extended"]:getSharedObject()
 
+---@return string
 function Fw_ESXBridge:GetPlayerName()
     local playerData = self.ESX.PlayerData
     return playerData.firstName .. ' ' .. playerData.lastName
