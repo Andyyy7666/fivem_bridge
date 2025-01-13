b8d65daf775002aa3017cba48cfcde3364e7c8e2
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2025-01-13 18:43:32 +0100

    tweak(frameworks/default/server): return types

--- frameworks/default/server.lua
+++ frameworks/default/server.lua
@@ -1,12 +1,14 @@
 Fw_Default = {}
 
 ---@param source string | number
+---@return string
 function Fw_Default:GetPlayerIdentifier(source)
     local identifierType = 'license' -- 'license', 'license2', 'steam', 'discord', 'ip', 'xbl', 'live'
     return GetPlayerIdentifierByType(source --[[@as string]], identifierType)
 end
 
 ---@param source string | number
+---@return string
 function Fw_Default:GetPlayerName(source)
     return GetPlayerName(source --[[@as string]])
 end
