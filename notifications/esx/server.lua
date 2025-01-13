c8283accc7cc76bd7c3e4b25271c11046959c7a2
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2025-01-13 18:49:06 +0100

    feat(notifications/esx/server): types

--- notifications/esx/server.lua
+++ notifications/esx/server.lua
@@ -1,5 +1,9 @@
 No_OxBridge = {}
 
+---@param source number
+---@param message string
+---@param type? 'info' | 'success' | 'error'
+---@param duration? number 
 function No_OxBridge:Notify(source, message, type, duration)
     TriggerClientEvent('bixbi_tracker:Bridge:Notify', source, message, type, duration)
 end
