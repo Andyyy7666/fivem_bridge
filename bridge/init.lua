e188885f76a2c13b5675ac340e8601df9639ca5c
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2025-01-14 23:15:23 +0100

    feat: load resource files

--- bridge/init.lua
+++ bridge/init.lua
@@ -1,3 +1,40 @@
+local function loadFile(modName)
+    -- load function taken from ox_lib https://github.com/overextended/ox_lib/blob/master/imports/require/shared.lua,
+    local path = './?.lua;./?/init.lua'
+    local tempData = {}
+
+    local function searchPath(name, path)
+        local resource = GetCurrentResourceName()
+        name = name:gsub('%.', '/')
+        local tried = {}
+
+        for template in path:gmatch('[^;]+') do
+            local fileName = template:gsub('^%./', ''):gsub('?', name)
+            local file = LoadResourceFile(resource, fileName)
+
+            if file then
+                tempData[1] = file
+                tempData[2] = resource
+                return fileName
+            end
+
+            tried[#tried + 1] = ("no file '@%s/%s'"):format(resource, fileName)
+        end
+
+        return nil, table.concat(tried, "\n\t")
+    end
+
+    local fileName, err = searchPath(modName, path)
+    if not fileName then
+        -- error(("file '%s' not found\n\t%s"):format(modName, err))
+        return
+    end
+
+    local file = tempData[1]
+    tempData[1] = nil
+    return assert(load(file, ('@@%s/%s'):format(tempData[2], fileName), 't', _ENV))()
+end
+
 local function getBridge(bridgeType)
     local context = IsDuplicityVersion() and "server" or "client"
 
@@ -11,10 +48,8 @@ local function getBridge(bridgeType)
 end
 
 Bridge = {
-    F = getBridge("frameworks"),
-    I = getBridge("inventories"),
-    N = getBridge("notifications")
-    T = getBridge("targets")
+    F = loadFile(getBridge("frameworks")),
+    I = loadFile(getBridge("inventories")),
+    N = loadFile(getBridge("notifications")),
+    T = loadFile(getBridge("targets"))
 }
-
--- todo: somehow load the bridge path like oxlib lib.load function.
