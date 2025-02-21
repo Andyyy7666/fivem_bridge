855718def42c691cbfac2e120da84f8fa3ee1629
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2025-02-21 22:50:02 +0100

    use oxlib instead of writing load code

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,12 @@ version "0.0.1"
 repository ""
 description ""
 
+files {
+    -- gotta add the client bridge files here that are loaded.
+}
+
 shared_scripts {
+    "@ox_lib/init.lua",
     "bridge/config.lua",
     "bridge/init.lua"
 }
