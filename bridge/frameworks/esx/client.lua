0baca47ac9c7b019f7b7d08de2f32e3ba9fa6a7e
GESUS <105702323+DevAlexandre0@users.noreply.github.com>
2025-02-23 21:01:03 +0700

    Update client.lua

--- bridge/frameworks/esx/client.lua
+++ bridge/frameworks/esx/client.lua
@@ -3,68 +3,68 @@ local ESX = exports["es_extended"]:getSharedObject()
 
 ---@return string
 function framework:GetPlayerName()
-    local playerData = ESX.PlayerData
-    return playerData.name
+    return ESX.GetPlayerData().name
 end
 
 ---@return string
 function framework:GetFirstName()
-    return ESX.PlayerData.firstName
+    return ESX.GetPlayerData().firstName
 end
 
 ---@return string
 function framework:GetLastName()
-    return ESX.PlayerData.lastName
+    return ESX.GetPlayerData().lastName
 end
 
 ---@return number
 function framework:GetCash()
-    return ESX.PlayerData.accounts.Money
+    return ESX.GetPlayerData().accounts['money']
 end
 
 ---@return number
 function framework:GetBank()
-    return ESX.PlayerData.accounts.Bank
+    return ESX.GetPlayerData().accounts['bank']
 end
 
 ---@return number
 function framework:GetBlackCash()
-    return ESX.PlayerData.accounts.Black
+    return ESX.GetPlayerData().accounts['black_money']
 end
 
 ---@return string
 function framework:GetDOB()
-    return ESX.PlayerData.dateofbirth
+    return ESX.GetPlayerData().dateofbirth
 end
 
 ---@return number
 function framework:GetJobGrade()
-    return ESX.PlayerData.job.grade
+    return ESX.GetPlayerData().job.grade
 end
 
 ---@return string
 function framework:GetJobGradeName()
-    return ESX.PlayerData.job.grade_name
+    return ESX.GetPlayerData().job.grade_name
 end
 
 ---@return string
 function framework:GetJobName()
-    return ESX.PlayerData.job.name
+    return ESX.GetPlayerData().job.name
 end
 
 ---@return string
 function framework:GetJobLabel()
-    return ESX.PlayerData.job.label
+    return ESX.GetJobLabel(ESX.GetPlayerData().job.name)
 end
 
----@return table
+---@return vector3
 function framework:GetCoords()
-    return ESX.PlayerData.coords
+    local playerPed = PlayerPedId()
+    return GetEntityCoords(playerPed)
 end
 
 ---@return string
 function framework:GetSex()
-    return ESX.PlayerData.sex
+    return ESX.GetPlayerData().sex
 end
 
 ---@return boolean
@@ -73,31 +73,61 @@ function framework:IsPlayerLoaded()
 end
 
 function framework:Progressbar(opts)
+    lib.progressBar({
+        duration = opts.duration or 5000,
+        label = opts.label or "Processing...",
+        useWhileDead = opts.useWhileDead or false,
+        canCancel = opts.canCancel or true,
+    })
 end
 
+---@param item string
+---@param count number
+---@return boolean
 function framework:SearchInventory(item, count)
+    local hasItem = exports.ox_inventory:Search('count', item) or 0
+    return hasItem >= (count or 1)
 end
 
+---@param key string
+---@param val any
 function framework:SetPlayerData(key, val)
+    ESX.SetPlayerData(key, val)
 end
 
 function framework:ShowAdvancedNotification(opts)
+    lib.notify({
+        title = opts.title or "Notification",
+        description = opts.description or "",
+        type = opts.type or "info"
+    })
 end
 
 function framework:ShowFloatingHelpNotification(opts)
+    lib.showTextUI(opts.text, {
+        position = opts.position or "top-center",
+        icon = opts.icon or "info"
+    })
 end
 
 function framework:ShowHelpNotification(opts)
+    AddTextEntry("HELP_NOTIFICATION", opts.text)
+    DisplayHelpTextThisFrame("HELP_NOTIFICATION", false)
 end
 
 function framework:ShowInventory()
+    exports.ox_inventory:openInventory('player')
 end
 
 function framework:ShowNotification(opts)
+    lib.notify({
+        description = opts.text or "",
+        type = opts.type or "info"
+    })
 end
 
-function framework:TriggerServerCallback(opts)
+function framework:TriggerServerCallback(event, cb, ...)
+    ESX.TriggerServerCallback(event, cb, ...)
 end
-
 
 return framework
