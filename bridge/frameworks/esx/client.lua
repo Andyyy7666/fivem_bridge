eb50b5ebf61a84938a9b3827e54659dbd7cd6cd7
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2025-03-04 01:37:18 +0100

    refactor: client framework bridge
    * removed bloat functions such as get coords (ox_lib cache.coords can instead be used)
    * Removed progressbars & notifications (will have own bridge folder)
    * Removed showinventory functions (will be in inventory bridge)
    * Removed server callback (use ox_lib callbacks instead it's better & less code here)
    * cleaned up & made everything much cleaner.

--- bridge/frameworks/esx/client.lua
+++ bridge/frameworks/esx/client.lua
@@ -1,135 +1,49 @@
 local framework = {}
 local ESX = exports["es_extended"]:getSharedObject()
 
----@return string
-function framework:GetPlayerName()
-    local playerData = ESX.PlayerData
-    return playerData.name
-end
-
----@return string
-function framework:GetFirstName()
-    return ESX.PlayerData.firstName
-end
-
----@return string
-function framework:GetLastName()
-    return ESX.PlayerData.lastName
-end
-
----@return number
-function framework:GetCash()
-    return ESX.PlayerData.accounts.Money
-end
-
----@return number
-function framework:GetBank()
-    return ESX.PlayerData.accounts.Bank
-end
-
----@return number
-function framework:GetBlackCash()
-    return ESX.PlayerData.accounts.Black
-end
-
----@return string
-function framework:GetDOB()
-    return ESX.PlayerData.dateofbirth
-end
-
----@return number
-function framework:GetJobGrade()
-    return ESX.PlayerData.job.grade
-end
-
----@return string
-function framework:GetJobGradeName()
-    return ESX.PlayerData.job.grade_name
-end
-
----@return string
-function framework:GetJobName()
-    return ESX.PlayerData.job.name
-end
-
----@return string
-function framework:GetJobLabel()
-    return ESX.PlayerData.job.label
-end
-
+---Get Player data
 ---@return table
-function framework:GetCoords()
-    return ESX.PlayerData.coords
+function framework:GetPlayer()
+    local player = ESX.PlayerData
+    return {
+        fullName = ("%s %s"):format(player.firstName, player.lastName),
+        firstName = player.firstName,
+        lastName = player.lastName,
+        dob = player.dateofbirth,
+        gender = player.sex
+    }
 end
 
----@return string
-function framework:GetSex()
-    return ESX.PlayerData.sex
+---Get any money/accounts
+---@param self any
+---@param type string
+---@return number
+function framework:GetMoney(self, type)
+    local player = ESX.PlayerData
+    if type == "cash" then
+        return player.accounts.Money
+    elseif type == "bank" then
+        return player.accounts.Bank
+    elseif type == "black" then
+        return player.accounts.Black
+    end
+end
+
+---Get all job info for the player
+---@return table
+function framework:GetJobInfo()
+    local player = ESX.PlayerData
+    return {
+        grade = player.job.grade,
+        gradeName = player.job.grade_name,
+        jobName = player.job.name,
+        jobLabel = player.job.label
+    }
 end
 
 ---@return boolean
 function framework:IsPlayerLoaded()
     return ESX.IsPlayerLoaded()
 end
-
-function framework:Progressbar(opts)
-    ESX.Progressbar(opts.message, opts.length, {
-        FreezePlayer = opts.FreezePlayer or false,
-        animation = opts.animation or nil,
-        onFinish = opts.onFinish or function() end,
-        onCancel = opts.onCancel or function() end
-    })
-end
-
-
-function framework:SearchInventory(item, count)
-    local xPlayer = ESX.GetPlayerData()
-    local inventory = xPlayer.inventory
-    for _, v in pairs(inventory) do
-        if v.name == item and v.count >= (count or 1) then
-            return true
-        end
-    end
-    return false
-end
-
-function framework:SetPlayerData(key, val)
-    ESX.SetPlayerData(key, val)
-end
-
-function framework:ShowAdvancedNotification(opts)
-    ESX.ShowAdvancedNotification(opts.sender, opts.subject, opts.message, opts.icon, opts.iconType or 0)
-end
-
-function framework:ShowFloatingHelpNotification(opts)
-    AddTextEntry('FloatingHelpNotification', opts.message)
-    SetFloatingHelpTextWorldPosition(1, opts.coords.x, opts.coords.y, opts.coords.z)
-    SetFloatingHelpTextStyle(1, 1, 2, -1, 3, 0)
-    BeginTextCommandDisplayHelp('FloatingHelpNotification')
-    EndTextCommandDisplayHelp(2, false, false, -1)
-end
-
-function framework:ShowHelpNotification(opts)
-    AddTextEntry('HelpNotification', opts.message)
-    BeginTextCommandDisplayHelp('HelpNotification')
-    EndTextCommandDisplayHelp(0, false, true, -1)
-end
-
-function framework:ShowInventory()
-    if ESX.UI.Menu.IsOpen('default', GetCurrentResourceName(), 'inventory') then
-        ESX.UI.Menu.Close('default', GetCurrentResourceName(), 'inventory')
-    else
-        ESX.ShowInventory()
-    end
-end
-
-function framework:ShowNotification(opts)
-    ESX.ShowNotification(opts.message, opts.type or 'info', opts.time or 5000)
-end
-
-function framework:TriggerServerCallback(name, cb, ...)
-    ESX.TriggerServerCallback(name, cb, ...)
-end
-
 
 return framework
--- bridge/frameworks/qb/client.lua
+++ bridge/frameworks/qb/client.lua
@@ -1,86 +1,53 @@
 local framework = {}
 local QBCore = exports["qb-core"]:GetCoreObject()
 
----@return string
-function framework:GetPlayerName()
-    local Player = QBCore.Functions.GetPlayerData()
-    local lastName = Player.charinfo.lastname
-    local firstName = Player.charinfo.firstname
-    return ("%s %s"):format(firstName, lastName)
-end
-
----@return string
-function framework:GetFirstName()
-    local player = QBCore.PlayerData?.charinfo
-    return player.firstname
-end
-
----@return string
-function framework:GetLastName()
-    local player = QBCore.PlayerData?.charinfo
-    return player.lastname
-end
-
----@return number
-function framework:GetCash()
-    return cashAmount
-end
-
----@return number
-function framework:GetBank()
-    return bankAmount
-end
-
----@return number
-function framework:GetBlackCash()
-    if GetResourceState("ox_inventory"):find("start") then
-        local <const> ox_inventory = exports.ox_inventory
-        local <const> usingDirtyMoney = ox_inventory:Items("black_money")
-        return ox_inventory:GetItemCount("black_money")
-    end
-    
-    return cashAmount
-end
-
----@return string
-function framework:GetDOB()
-    local player = QBCore.PlayerData?.charinfo
-    return player.birthdate
-end
-
----@return number
-function framework:GetJobGrade()
-    local player = QBCore.PlayerData
-    return player.job.grade.level
-end
-
----@return string
-function framework:GetJobGradeName()
-    local player = QBCore.PlayerData
-    return player.job.grade.name
-end
-
----@return string
-function framework:GetJobName()
-    local player = QBCore.PlayerData
-    return player.job.name
-end
-
----@return string
-function framework:GetJobLabel()
-    local player = QBCore.PlayerData
-    return player.job.label
-end
-
+---Get Player data
 ---@return table
-function framework:GetCoords()
-    return cache.coords or GetEntityCoords(cache.ped)
+function framework:GetPlayer()
+    local player = QBCore.Functions.GetPlayerData()
+    local info = player.charinfo
+    local lastName = info.lastname
+    local firstName = info.firstname
+    return {
+        fullName = ("%s %s"):format(firstName, lastName),
+        firstName = player.firstname,
+        lastName = player.lastname,
+        dob = info.birthdate,
+        gender = info.gender
+    }
 end
 
----@return string
-function framework:GetSex()
-    local player = QBCore.PlayerData?.charinfo
-    return player.gender
+---Get any money/accounts
+---@param self any
+---@param type string
+---@return number
+function framework:GetMoney(self, type)
+    local player = QBCore.Functions.GetPlayerData()
+    if type == "cash" then
+        return player.money["cash"]
+    elseif type == "bank" then
+        return player.money["bank"]
+    elseif type == "black" then
+        return player.money["cash"] -- note: need to figure this out.
+    end
+end
+
+---Get all job info for the player
+---@return table
+function framework:GetJobInfo()
+    local player = QBCore.Functions.GetPlayerData()
+    local job = player.job
+    return {
+        grade = job.grade.level,
+        gradeName = job.grade.name,
+        jobName = job.name,
+        jobLabel = job.label
+    }
+end
+
+---@return boolean
+function framework:IsPlayerLoaded()
+    return QBCore.Functions.GetPlayerData() ~= nil
 end
 
 return framework
